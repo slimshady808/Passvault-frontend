9095cfd560ff46de991e2bedcb81d684e13a34f5
akshay <akshay@gmail.com>
2023-09-28 21:35:48 +0530

    modal button

--- src/components/PasswordModal.jsx
+++ src/components/PasswordModal.jsx
@@ -32,7 +32,7 @@ export const PasswordModal = () => {
   return (
     <div>
       <button
-        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
         onClick={openModal}
       >
         Generate Password
