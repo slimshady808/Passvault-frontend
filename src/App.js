27e1eb40988928bb4c7edc92b5074dbf4fc26e9c
akshay <akshay@gmail.com>
2023-10-02 09:17:32 +0530

    not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { HomePage } from './pages/HomePage';
 import { SignupPage } from './pages/SignupPage';
 import { LoginPage } from './pages/LoginPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route Component={HomePage} path='/'/>
         <Route Component={SignupPage} path='/signup'/>
         <Route Component={LoginPage} path='/login'/>
-        
+        <Route path="*" element={<NotFoundPage />} />
       
       </Routes>
     </Router>
