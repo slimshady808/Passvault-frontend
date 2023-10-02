c2a6a342d9214d3da21352150ffcfadaf6449ba1
akshay <akshay@gmail.com>
2023-10-02 06:15:50 +0530

    server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server='http://127.0.0.1:8000'
-
+// export const server='http://127.0.0.1:8000'
+export const server ='https://passvaultz.onrender.com'
 
 
 // process.env.REACT_APP_SERVER;
